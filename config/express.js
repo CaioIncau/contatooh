aadc21efff5628e4c222e52c5404ea3cca23b0d5
Caio Incau <caioincau@gmail.com>
2015-03-08 15:56:00 -0300

    grunt applied

--- config/express.js
+++ config/express.js
@@ -41,5 +41,9 @@ module.exports = function() {
     .then('controllers')
     .then('routes')
     .into(app);
+
+  app.get('*', function(req, res) {
+    res.status(404).render('404');
+  });
   return app;
 };
